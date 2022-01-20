4b4ed5d82ca9e05b11096fa7ecbc1017c245502f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-20 09:15:01 +0700

    add Calculator class & test

---
+++ src/main/java/rifqimuhammadaziz/test/Calculator.java
@@ -0,0 +1,8 @@
+package rifqimuhammadaziz.test;
+
+public class Calculator {
+
+    public Integer add(Integer number1, Integer number2) {
+        return number1 + number2;
+    }
+}
---
+++ src/test/java/rifqimuhammadaziz/test/CalculatorTest.java
@@ -0,0 +1,15 @@
+package rifqimuhammadaziz.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddSuccess() {
+        var result = calculator.add(100, 20);
+    }
+
+
+}
