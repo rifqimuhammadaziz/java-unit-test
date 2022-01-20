b760b89fb4fa66eca6f8521189b602bd58cd6377
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-20 09:45:04 +0700

    add test & create generator for display test name

--- src/main/java/rifqimuhammadaziz/test/Calculator.java
+++ src/main/java/rifqimuhammadaziz/test/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
     public Integer add(Integer number1, Integer number2) {
         return number1 + number2;
     }
+
+    public Integer divide(Integer number1, Integer number2) {
+        if (number1 == 0 || number2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        } else {
+            return number1 / number2;
+        }
+    }
 }
--- src/test/java/rifqimuhammadaziz/test/CalculatorTest.java
+++ src/test/java/rifqimuhammadaziz/test/CalculatorTest.java
@@ -1,15 +1,46 @@
 package rifqimuhammadaziz.test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
+import rifqimuhammadaziz.test.generator.SimpleDisplayNameGenerator;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
+    // @DisplayName("Test Success for method add(Integer, Integer)")
     public void testAddSuccess() {
         var result = calculator.add(100, 20);
+        assertEquals(120, result);
     }
 
+    @Test
+    // @DisplayName("Test Failed for method add(Integer, Integer)")
+    public void testAddFailed() {
+        var result = calculator.add(100, 20);
+        assertNotEquals(1, result);
+    }
+
+    @Test
+    // @DisplayName("Test Success for method divide(Integer, Integer)")
+    public void testDivideSuccess() {
+        var result = calculator.divide(100, 20);
+        assertEquals(5, result);
+    }
+
+    @Test
+    // @DisplayName("Test Failed for method divide(Integer, Integer)")
+    public void testDivideFailed() {
+        // if running on main class it will throw exception
+        // For test, the method must throw an error
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(100, 0);
+        });
+    }
 
 }
