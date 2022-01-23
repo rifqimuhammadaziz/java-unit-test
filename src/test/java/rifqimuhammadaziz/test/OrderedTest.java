806ac138676eefa9fe1c796cd95b1651d091a102
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-23 22:55:32 +0700

    add OrderedTest

---
+++ src/test/java/rifqimuhammadaziz/test/OrderedTest.java
@@ -0,0 +1,34 @@
+package rifqimuhammadaziz.test;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class OrderedTest {
+
+    @Test
+    void test5() {
+
+    }
+
+    @Test
+    void test4() {
+
+    }
+
+    @Test
+    void test3() {
+
+    }
+
+    @Test
+    void test2() {
+
+    }
+
+    @Test
+    void test1() {
+
+    }
+}
