514a0c608361aa7c5d776707b365e5cc38f744a5
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-24 10:00:38 +0700

    add dependency injection on test

---
+++ src/test/java/rifqimuhammadaziz/test/RandomCalculatorTest.java
@@ -0,0 +1,27 @@
+package rifqimuhammadaziz.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import rifqimuhammadaziz.test.resolver.RandomParameterResolver;
+
+import java.util.Random;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class RandomCalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+        var result = calculator.add(a, b);
+        var expected = a +b ;
+
+        Assertions.assertEquals(expected, result);
+    }
+}
