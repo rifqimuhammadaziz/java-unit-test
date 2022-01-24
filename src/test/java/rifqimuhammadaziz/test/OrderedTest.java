7a8afaf259a8e6111db4f36969326776461e0369
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-24 09:12:33 +0700

    add OrderAnnotation on test

--- src/test/java/rifqimuhammadaziz/test/OrderedTest.java
+++ src/test/java/rifqimuhammadaziz/test/OrderedTest.java
@@ -1,33 +1,38 @@
 package rifqimuhammadaziz.test;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
 
     @Test
+    @Order(2)
     void test5() {
 
     }
 
     @Test
+    @Order(1)
     void test4() {
 
     }
 
     @Test
+    @Order(4)
     void test3() {
 
     }
 
-    @Test
+    @Test@Order(5)
     void test2() {
 
     }
 
     @Test
+    @Order(3)
     void test1() {
 
     }
