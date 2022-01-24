65b987c5fcf0812df12495008f875ec4c8334c1a
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-24 10:31:01 +0700

    inherit test

---
+++ src/test/java/rifqimuhammadaziz/test/AbstractCalculatorTest.java
@@ -0,0 +1,19 @@
+package rifqimuhammadaziz.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import rifqimuhammadaziz.test.resolver.RandomParameterResolver;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class AbstractCalculatorTest {
+
+    protected Calculator calculator = new Calculator();
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before each");
+    }
+}
